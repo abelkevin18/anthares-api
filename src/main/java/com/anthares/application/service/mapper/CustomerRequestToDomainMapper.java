0b35a9aabc4d11962b61018ae3f74a7d9a62f1a4
Abel Nunez <abelkevin13@gmail.com>
2026-01-31 22:38:33 -0500

    refactor: update mappers to simplify ID generation and improve structure

--- src/main/java/com/anthares/application/service/mapper/CustomerRequestToDomainMapper.java
+++ src/main/java/com/anthares/application/service/mapper/CustomerRequestToDomainMapper.java
@@ -2,9 +2,13 @@ package com.anthares.application.service.mapper;
 
 import com.anthares.application.dto.request.CustomerRequest;
 import com.anthares.domain.model.Customer;
+import com.anthares.commons.util.IdGenerator;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", imports = {IdGenerator.class})
 public interface CustomerRequestToDomainMapper {
+
+  @Mapping(target = "id", expression = "java(IdGenerator.generateId())")
   Customer fromRequest(CustomerRequest customerRequest);
 }
--- src/main/java/com/anthares/infrastructure/adapter/in/web/mapper/domain/CustomerDomainToResponseMapper.java
+++ src/main/java/com/anthares/infrastructure/adapter/in/web/mapper/domain/CustomerDomainToResponseMapper.java
@@ -3,13 +3,11 @@ package com.anthares.infrastructure.adapter.in.web.mapper.domain;
 import com.anthares.domain.model.Customer;
 import com.anthares.infrastructure.adapter.out.persistence.entity.CustomerJpaEntity;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring", uses = {IdMapper.class})
+@Mapper(componentModel = "spring")
 public interface CustomerDomainToResponseMapper {
 
   Customer toDomain(CustomerJpaEntity dao);
 
-  @Mapping(target = "id", source = "id", qualifiedByName = "generateId")
   CustomerJpaEntity toDao(Customer customer);
 }
